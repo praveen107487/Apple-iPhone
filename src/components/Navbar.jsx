3846a6bbaac1b4307bfb927acc1bbd7ef82df386
praveen107487 <praveen107487@gmail.com>
2026-02-19 19:51:35 +0530

    Hero Section Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,29 @@
-import { appleImg, bagImg, searchImg } from '../utils';
-import { navLists } from '../constants';
+import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full screen-max-width">
+    <header className="w-full h-[60px] sm:px-10 px-5 flex items-center">
+      <nav className="flex w-full screen-max-width items-center">
+        {/* Apple Logo */}
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
-        <div className="flex flex-1 justify-center max-sm:hidden">
+        {/* Center Nav Items */}
+        <div className="flex flex-1 justify-center gap-8 max-sm:hidden">
           {navLists.map((nav) => (
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer text-gray opacity-80 hover:opacity-100 hover:text-white transition-colors duration-300" >
+              className="text-sm cursor-pointer text-gray opacity-80
+                         hover:opacity-100 hover:text-white
+                         transition-colors duration-300"
+            >
               {nav}
             </div>
-
           ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+        {/* Right Icons */}
+        <div className="flex items-center gap-7 max-sm:flex-1 max-sm:justify-end">
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
